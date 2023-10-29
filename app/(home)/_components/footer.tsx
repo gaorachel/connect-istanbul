97a5f980c385b3566825289c5a668c5e666544f7
Rachel <gggggrea@gmail.com>
2023-10-29 23:07:24 +0000

    Add mobile mode for Footer

--- app/(home)/_components/footer.tsx
+++ app/(home)/_components/footer.tsx
@@ -1,19 +1,49 @@
-import { Button } from "@/components/button";
-import { Facebook, Instagram, Languages } from "lucide-react";
+"use client";
+
+import { useMediaQuery } from "usehooks-ts";
+import { Facebook, Instagram } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { Button } from "@/components/button";
+
 export const Footer = () => {
+  const isWebSize = useMediaQuery("(min-width: 768px)");
+
   return (
     <footer className="flex flex-col bg-zinc-800 justify-center">
       <div className="p-5 m-auto">
         <Button label="Contact us ->" href="contact-us" variant="light" type="button" />
       </div>
-      <div className="p-2 flex flex-row justify-center space-x-8 bg-zinc-100">
-        <Image src="/qr-code.png" width={150} height={150} alt="QR CODE" className="object-contain" />
-        <Image src="/yedab.png" width={250} height={100} alt="YEDAB logo" className="object-contain" />
-        <Image src="/ielts.png" width={250} height={100} alt="IELTS logo" className="object-contain" />
-        <Image src="/pearson.png" width={250} height={100} alt="Pearson logo" className="object-contain" />
+      <div className="p-2 flex flex-col md:flex-row space-0  md:space-x-8 justify-center items-center bg-zinc-100">
+        <Image
+          src="/qr-code.png"
+          width={isWebSize ? 150 : 75}
+          height={isWebSize ? 150 : 75}
+          alt="QR CODE"
+          className="object-contain"
+        />
+        <Image
+          src="/yedab.png"
+          width={isWebSize ? 250 : 150}
+          height={isWebSize ? 100 : 75}
+          alt="YEDAB logo"
+          className="object-contain"
+        />
+        <Image
+          src="/ielts.png"
+          width={isWebSize ? 250 : 150}
+          height={isWebSize ? 100 : 75}
+          alt="IELTS logo"
+          className="object-contain"
+        />
+        <Image
+          src="/pearson.png"
+          width={isWebSize ? 250 : 150}
+          height={isWebSize ? 100 : 75}
+          alt="Pearson logo"
+          className="object-contain"
+        />
       </div>
       <div className="flex flex-row justify-end p-2 pr-20 space-x-5">
         <Link href="https://www.instagram.com/connect.istanbul">
@@ -23,7 +53,7 @@ export const Footer = () => {
           <Facebook size={50} color="white" className="p-2 hover:bg-[#0966FF] hover:rounded-full" />
         </Link>
       </div>
-      <p className="text-white p-4 m-auto"> © 2023 Connect Istanbul. All rights reserved. </p>
+      <p className="text-white p-4 m-auto text-sm md:text-base"> © 2023 Connect Istanbul. All rights reserved. </p>
     </footer>
   );
 };
