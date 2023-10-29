1308a65a377d2d9acd6bd3158279219d35d7bbba
Rachel <gggggrea@gmail.com>
2023-10-29 21:05:51 +0000

    Create Button component

---
+++ components/button.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+import { VariantProps, cva } from "class-variance-authority";
+
+const buttonVariants = cva("p-4 px-8 font-semibold text-white hover:text-white", {
+  variants: {
+    variant: {
+      light: "bg-white text-zinc-700 hover:bg-black hover:text-white",
+      dark: "bg-zinc-600 hover:bg-white hover:text-zinc-600",
+      red: "bg-red-500 hover:bg-red-700",
+    },
+    type: {
+      button: "button",
+      submit: "submit",
+    },
+  },
+  defaultVariants: {
+    variant: "light",
+    type: "button",
+  },
+});
+
+type ButtonVariantsProps = VariantProps<typeof buttonVariants>;
+
+interface ButtonProps {
+  label: string;
+  href: string;
+  customisedStyle?: string;
+}
+
+export const Button = ({ label, href, variant, type }: ButtonProps & ButtonVariantsProps) => {
+  const router = useRouter();
+
+  return (
+    <button onClick={() => router.push(href)} className={cn(buttonVariants({ variant, type }))}>
+      {label}
+    </button>
+  );
+};
