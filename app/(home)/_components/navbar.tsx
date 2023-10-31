50a9d16ff9b6f81825889b0cf2e4319358c3c53a
Rachel <gggggrea@gmail.com>
2023-10-31 21:55:27 +0000

    Change design

--- app/(home)/_components/navbar.tsx
+++ app/(home)/_components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Instagram, Facebook, Calculator } from "lucide-react";
+import { Instagram, Facebook } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 
@@ -15,17 +15,12 @@ const routes = [
   { label: "Contact us", href: "/contact-us" },
 ];
 
-const modals = [
-  { label: "Language Price Offer", href: "/language-price-offer" },
-  { label: "University Price Offer", href: "/university-price-offer" },
-];
-
-interface ItemProps {
+interface NavbarItemProps {
   label: string;
   href: string;
 }
 
-const NavbarItem = ({ label, href }: ItemProps) => {
+const NavbarItem = ({ label, href }: NavbarItemProps) => {
   const pathname = usePathname();
   const router = useRouter();
 
@@ -36,7 +31,7 @@ const NavbarItem = ({ label, href }: ItemProps) => {
       type="button"
       onClick={() => router.push(href)}
       className={cn(
-        "p-3 md:pb-2 md:mb-1 md:mr-2 text-md font-semibold text-zinc-500 hover:text-zinc-800",
+        "p-3 md:pb-2 text-md font-semibold text-zinc-500 hover:text-zinc-800",
         isActive &&
           "text-zinc-800 border-red-400 w-full bg-red-200/20 md:border-b-4 md:border-r-0 border-r-4 md:w-auto md:bg-transparent"
       )}
@@ -46,41 +41,22 @@ const NavbarItem = ({ label, href }: ItemProps) => {
   );
 };
 
-const ModalItem = ({ label, href }: ItemProps) => {
-  <button type="button">
-    {label} {/* TODO: create modals */}
-  </button>;
-};
-
 export const Navbar = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col md:mr-5">
       <div className="pt-10 md:pt-3 flex flex-col items-start md:flex-row">
         {routes.map((route) => (
           <NavbarItem label={route.label} href={route.href} key={route.href} />
         ))}
       </div>
 
-      <div className="flex flex-col md:flex-row p-2 justify-between">
-        <div className="flex flex-col md:flex-row md:items-center md:space-x-3 cursor-pointer">
-          {modals.map((modal) => (
-            <div
-              key={modal.href}
-              className="py-4 md:p-2 gap-2 flex flex-row md:rounded-full text-zinc-500 md:border-2 md:border-zinc-400 hover:text-zinc-700 hover:border-zinc-700 hover:border-3"
-            >
-              {<Calculator color="#71717a" className="hover:text-zinc-700" />} {modal.label}
-            </div>
-          ))}
-        </div>
-
-        <div className="flex flex-row space-x-3 py-4 ml-5 md:p-0 md:m-0">
-          <Link href="https://www.instagram.com/connect.istanbul">
-            <Instagram size={45} className="p-2 hover:bg-[#CF2872] hover:rounded-full" />
-          </Link>
-          <Link href="https://www.facebook.com/connectistanbul/">
-            <Facebook size={45} className="p-2 hover:bg-[#0966FF] hover:rounded-full" />
-          </Link>
-        </div>
+      <div className="flex flex-row space-x-3 justify-center py-4 md:justify-end ">
+        <Link href="https://www.instagram.com/connect.istanbul">
+          <Instagram size={45} className="p-2 hover:bg-[#CF2872] hover:rounded-full" />
+        </Link>
+        <Link href="https://www.facebook.com/connectistanbul/">
+          <Facebook size={45} className="p-2 hover:bg-[#0966FF] hover:rounded-full" />
+        </Link>
       </div>
     </div>
   );
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,24 @@ import hero2 from "@/public/home/hero2.jpg";
 
 import { Button } from "@/components/button";
 import { Separator } from "@/components/ui/separator";
+const modals = [
+  { label: "Check Language Course Fees ", href: "/language-price-Check" },
+  { label: "Check University Tuition Fees", href: "/university-price-Check" },
+];
+
+interface ModalItemProps {
+  label: string;
+}
+
+const ModalItem = ({ label }: ModalItemProps) => {
+  return (
+    <div className="flex flex-row p-2 px-4 w-[300px] rounded-full gap-2 text-zinc-700 border-2 bg-zinc-200 border-zinc-700 hover:text-zinc-500 hover:border-zinc-500 hover:border-3 hover:bg-zinc-100/80 transition">
+      {<Calculator color="#3f3f46" />}
+      <button onClick={() => {}}>{label}</button>
+      {/* TODO: create modals */}
+    </div>
+  );
+};
 
 export default function Home() {
   return (
@@ -13,11 +31,15 @@ export default function Home() {
           alt="graduation celebration"
           src={hero1}
           className="-z-50  w-full h-auto md:h-[400px] overflow-hidden object-cover"
-          placeholder="blur"
+          // placeholder="blur"
         />
-        <div className="absolute top-0 p-10 w-full h-full md:h-[200px] md:mt-24 text-center space-y-6 bg-zinc-200/80">
-          <h1 className="font-semibold md:text-3xl text-2xl"> Your gateway to study overseas </h1>
-          <Button label="About us ->" href="about-us" />
+        <div className="absolute top-0 p-10 space-x-3 w-full h-full md:h-[200px] md:mt-24 bg-zinc-200/80">
+          <h1 className="font-semibold md:text-3xl text-2xl text-center"> Your gateway to study overseas </h1>
+          <div className="p-14 md:p-10 flex flex-col md:flex-row cursor-pointer space-y-5 md:space-y-0 items-center md:justify-center md:gap-5">
+            {modals.map((modal) => (
+              <ModalItem key={modal.label} label={modal.label} />
+            ))}
+          </div>
         </div>
       </div>
 
@@ -30,7 +52,7 @@ export default function Home() {
           alt="London Big Ben & Westminster Palace"
           src={hero2}
           className="-z-50 w-full h-auto md:h-[400px] overflow-hidden object-cover"
-          placeholder="blur"
+          // placeholder="blur"
         />
         <div className="absolute top-0 p-10 md:ml-10 md:w-[400px] h-full w-full text-center space-y-6 bg-zinc-800/60">
           <h1 className="font-semibold md:text-3xl text-2xl text-white">
