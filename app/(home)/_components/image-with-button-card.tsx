2a2fd6f40c8d264bf6e811e637241cf540cd236d
Rachel <gggggrea@gmail.com>
2023-11-02 19:59:44 +0000

    Redesign programmeCards

--- app/(home)/_components/image-with-button-card.tsx
+++ app/(home)/_components/image-with-button-card.tsx
@@ -6,15 +6,16 @@ import { DivideSquare, ExternalLink, Link, Link2 } from "lucide-react";
 interface ImageWithButtonCardProps {
   label: string;
   code: string;
+  type: string;
 }
 
-export const ImageWithButtonCard = ({ label, code }: ImageWithButtonCardProps) => {
+export const ImageWithButtonCard = ({ label, code, type }: ImageWithButtonCardProps) => {
   return (
-    <div className="rounded-md flex flex-col shadow-xl h-[350px] w-[300px] cursor-pointer">
-      <Image src={`/home/${code}.jpg`} alt={label} height={300} width={300} className="rounded-t-lg h-[220px] w-auto" />
-      <p className="text-lg font-semibold p-2 py-10 flex flex-row gap-2 m-auto">
-        {label} <ExternalLink size={20} />
-      </p>
+    <div className="rounded-xl flex flex-col shadow-xl h-[350px] w-[300px] cursor-pointer hover:-translate-y-5 transition-all">
+      <Image src={`/home/${code}.jpg`} alt={label} height={300} width={300} className="rounded-t-xl h-[220px] w-auto" />
+      <p className="p-4 py-4 text-sm text-red-400 font-semibold"> {type.toUpperCase()} </p>
+      <h2 className="pl-4 px-auto text-xl font-semibold flex flex-row gap-2">{label}</h2>
+      <p></p>
     </div>
   );
 };
--- app/(home)/_components/programme-cards.tsx
+++ app/(home)/_components/programme-cards.tsx
@@ -4,34 +4,42 @@ const programmes = [
   {
     label: "Summer Schools",
     code: "summer-schools",
+    type: "Short term",
   },
   {
     label: "Language Schools",
     code: "language-schools",
+    type: "Short term",
   },
   {
     label: "Undergraduate",
     code: "undergraduate",
+    type: "Long term",
   },
   {
     label: "Postgraduate",
     code: "postgraduate",
+    type: "Long term",
   },
   {
     label: "High Schools",
     code: "high-schools",
+    type: "Long term",
   },
   {
     label: "CELTA",
     code: "celta",
+    type: "Short term",
   },
   {
     label: "50+ Programmes",
     code: "50plus-programmes",
+    type: "Short term",
   },
   {
     label: "Gallery",
     code: "gallery",
+    type: "References",
   },
 ];
 
@@ -43,7 +51,12 @@ export const ProgrammeCards = () => {
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 px-24 p-10 gap-10 rounded-lg">
         {programmes.map((programme) => (
-          <ImageWithButtonCard label={programme.label} code={programme.code} key={programme.code} />
+          <ImageWithButtonCard
+            label={programme.label}
+            code={programme.code}
+            type={programme.type}
+            key={programme.code}
+          />
         ))}
       </div>
     </div>
