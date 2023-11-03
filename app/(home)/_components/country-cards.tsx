4ac2635fd4ab4518650f3335d7342a8c903775b9
Rachel <gggggrea@gmail.com>
2023-11-03 18:36:21 +0000

    Add fixed width for cards

--- app/(home)/_components/country-cards.tsx
+++ app/(home)/_components/country-cards.tsx
@@ -49,7 +49,7 @@ export const CountryCards = () => {
   return (
     <div>
       <p className="p-5 md:p-20 text-3xl text-center font-bold"> Education in many countries </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 px-24 p-10 gap-10 rounded-lg">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-24 p-10 gap-10 rounded-lg lg:w-[1400px] md:w-[800p] m-auto">
         {countries.map((country) => (
           <ImageCard {...country} key={country.code} />
         ))}
--- app/(home)/_components/programme-cards.tsx
+++ app/(home)/_components/programme-cards.tsx
@@ -51,7 +51,7 @@ export const ProgrammeCards = () => {
       <p className="p-5 md:p-20 text-3xl text-center font-bold">
         We have various programmes for different aohfodhfihsdlkghlsdg.
       </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 px-24 p-10 gap-10 rounded-lg">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-24 p-10 gap-10 rounded-lg lg:w-[1400px] md:w-[800p] m-auto">
         {programmes.map((programme) => (
           <ImageCard {...programme} key={programme.code} />
         ))}
