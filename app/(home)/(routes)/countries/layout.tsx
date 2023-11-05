36a9b858a29a8400010966361c05ad71a1e4433e
Rachel <gggggrea@gmail.com>
2023-11-05 12:20:01 +0000

    Add mobile table of content for countries

--- app/(home)/(routes)/countries/layout.tsx
+++ app/(home)/(routes)/countries/layout.tsx
@@ -1,19 +1,24 @@
 "use client";
 
+import { useMediaQuery } from "@react-hook/media-query";
+
 import { countries } from "../../_components/country-cards";
-import { TableOfContent } from "../../../../components/table-of-content";
+import { MobileTableOfContent } from "@/components/mobile-table-of-content";
+import { TableOfContent } from "@/components/table-of-content";
 
 import type { ReactNode } from "react";
 
-const CountriesLayout = ({ children }: { children: ReactNode }) => {
+const ProgrammesLayout = ({ children }: { children: ReactNode }) => {
+  const isMediumDevice = useMediaQuery("only screen and (min-width: 768px)");
+
   return (
-    <div className="flex flex-row w-[1200px] m-auto">
-      <aside>
-        <TableOfContent items={countries} slug="countries" />
-      </aside>
+    <div className="flex flex-col md:flex-row md:w-[1200px] m-auto">
+      {!isMediumDevice && <MobileTableOfContent items={countries} slug="countries" />}
+      {isMediumDevice && <TableOfContent items={countries} slug="countries" />}
+
       <main> {children} </main>
     </div>
   );
 };
 
-export default CountriesLayout;
+export default ProgrammesLayout;
