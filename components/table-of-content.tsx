c8956bf9da9750415254fc6c75328a3b20e1ec4b
Rachel <gggggrea@gmail.com>
2023-11-11 16:38:55 +0000

    Update TableOfContent style

--- components/table-of-content.tsx
+++ components/table-of-content.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
+import { ChevronDown } from "lucide-react";
 
 interface TableOfContentProps {
   items: {
@@ -17,19 +18,24 @@ export const TableOfContent = ({ items, slug }: TableOfContentProps) => {
   const selectedItem = pathname.split("/").pop();
 
   return (
-    <div className="p-10 text-lg space-y-5">
-      {items.map((item) => (
-        <Link
-          className={cn(
-            "flex flex-row cursor-pointer text-zinc-500 hover:text-zinc-800",
-            selectedItem === item.code && "text-zinc-800 font-semibold translate-x-10 transition"
-          )}
-          key={item.code}
-          href={`/${slug}/${item.code}`}
-        >
-          {item.label}
-        </Link>
-      ))}
+    <div className="bg-zinc-50">
+      <span className="flex flex-row gap-2 w-[250px] border-y p-2 px-4 m-0 text-xl font-semibold">
+        <ChevronDown /> Table of Content
+      </span>
+      <div className="p-10 text-lg space-y-5 h-full overflow-y-scroll min-w-fit">
+        {items.map((item) => (
+          <Link
+            className={cn(
+              "flex flex-row cursor-pointer text-zinc-500 hover:text-zinc-800",
+              selectedItem === item.code && "text-zinc-800 font-semibold translate-x-10 transition"
+            )}
+            key={item.code}
+            href={`/${slug}/${item.code}`}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
