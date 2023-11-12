7eb7702cb1b041d4890ab5d5a04c74e470a3fd62
Rachel <gggggrea@gmail.com>
2023-11-12 11:17:56 +0000

    Refactor input data storage structure

---
+++ app/(home)/(routes)/programmes/_data/programmeInfo.ts
@@ -0,0 +1,266 @@
+export const programmes = [
+  {
+    label: "Summer Schools",
+    code: "summer-schools",
+    type: "Short term",
+    tag: "popular",
+    desc: `Summer schools are an opportunity, at the end of an intensive education period, where students can learn a language they want to learn other than their mother tongue by attending summer camps abroad. These summer camps are very effective in helping students develop their self-confidence, become responsible, and set a goal at an early age. It is extremely important for their personal development that they can make friends with students from different countries, get to know different cultures and practice with these people.
+
+ 
+
+      Summer schools can be scheduled for a minimum of 2 weeks and a maximum of 3 months in June-July-August. The age groups for these programs are as follows:
+      
+      12-17 years old : Students receive education and stay on school campuses. They classify according to their level, with a maximum of 15 people in a class, and whenever possible, a maximum of 1 person from each nationality. The education is half-day, and in the afternoon, they participate in excursions and activities programmed by the school with other students, accompanied by their school teachers. 
+      
+       
+      
+      Countries and cities we work in,
+      
+      İngiltere (London, Oxford, Bournemouth, Brighton, Cambridge, Canterbury)
+      
+      Canada (Toronto, Vancouver)
+      
+      America (Los Angeles, Boston)
+      
+      Malta (Sliema)
+      
+       
+      
+      Considering the feedback we have received from our students who have previously participated in these programs, we can say that these camps have the potential to be a turning point in the students' lives, both in terms of their worldview and broadening their horizons. 
+      
+       
+      
+      You can always contact our consultants for information or for your questions.`,
+  },
+  {
+    label: "Language Schools",
+    code: "language-schools",
+    type: "Short term",
+    tag: "popular",
+    desc: `"Language is the road map of a culture. It shows where people come from and where they are going." -Rita Mae Brown
+
+    The easiest way to learn a language is to learn it by living in the country where this language is spoken. Even someone who does not know a foreign language can easily reach the desired level in a short time, if he receives the right education. At the same time, while receiving this education, being in touch with the culture where the language was born and raised provides you with personal development.
+    
+     
+    
+    Programs of language schools are minimum 2 weeks and maximum 1 year. Anyone over the age of 16 has the opportunity to attend language schools.
+    
+    Students stay in homestays or dormitories. Lesson hours vary depending on preference, from at least 15 hours to 30 hours per week. Students are classified according to their level, with a maximum of 15 people in a class, and a maximum of 1 person from each nationality as possible.
+    
+    The given program consists of grammar, speaking lessons and visual and auditory practices given in language laboratories. In addition to this language training, different training programs such as business English, exam preparation programs and one-on-one lessons are also offered.
+    
+     
+    
+    The countries and cities we work in,
+    
+    İngiltere (London, Oxford, Bournemouth, Brighton, Cambridge, Canterbury, Bath)
+    
+    Ireland (Dublin)
+    
+    Scotland (Edinburgh)
+    
+    Canada (Toronto, Vancouver, Calgary)
+    
+    Amerika (Los Angeles, San Francisco, Miami, Boston Washington DC)
+    
+    Malta (Sliema)
+    
+     
+    
+    You can always contact our consultants for information or for your questions.`,
+  },
+  {
+    label: "Undergraduate",
+    code: "undergraduate",
+    type: "Long term",
+    desc: `University education abroad has been one of the main areas of education abroad for students in our country for years. When it comes to university education abroad, the first countries that come to mind are America, Canada, England, Germany and recently Poland. There are many reasons why students prefer university education abroad in these countries. One of the most important reasons expressed by most students is the opportunity to live abroad while receiving a modern university education.
+
+    The biggest advantage of students who will receive university education abroad is that they have the opportunity to study in the department of their choice in many countries without the pressure of exams. Students can be accepted to the department they are interested in and want, with the grades they have received throughout their education, their social projects, volunteer services, and the talents and skills they have developed, without having to take a compulsory and one-time exam. Another of the biggest advantages of studying at a university abroad is to effectively master the foreign language. is to have learned. In addition, students who study at a university abroad have the opportunity to learn other languages ​​in the country they visit. In this case, students acquire important language skills that they can use in their business and social lives.
+    
+    While students receive university education abroad, in some countries they do not have to take a preparatory program plus a 4-year university education, as in Turkey. Instead, students can receive a university education within a 3-year period. In this case, it is preferred for many students. While students later have the opportunity to specialize in the continuation of their university education with master's, certificate or diploma programs, they can enter business life more professionally 1 year earlier than other students. Students who receive both shorter and lower-cost education can also go to schools with YÖK accreditation and continue their business life without any problems when they return to Turkey. Even though university education abroad varies from country to country, it is often not more expensive than in Turkey. While students have the opportunity to specialize in a language, live and do an internship in a country abroad, they have the opportunity to do this at the same costs as in Turkey. That's why the majority of students keep universities abroad at the top of their education preferences. Because the education structure and cost of many universities abroad, which have YÖK equivalence and accept students without examination, are no different from the schools in Turkey.
+    
+     
+    
+    Entry Conditions
+    Entry requirements for universities for university education abroad vary from country to country, but generally applied conditions are:
+    
+     Adequate Grade Point Average (High School Graduation Average/GPA)
+    
+     Applying to a department in fields parallel to the department studied at the university
+    
+     To show sufficient success in the language exam determined by the school where the master's degree will be received (IELTS / TOEFL / DELE / TestDAF etc.)
+    
+     Demonstrate the required success in the proficiency exams determined by the school where the master's degree will be received (SAT and school-specific exams)
+    
+     Reference Letters (Received from the educational institution you graduated from or your last workplace)
+    
+     Letter of Intent
+    
+     Candidate's CV
+    
+     The candidate's previous achievements, awards and competition results
+    
+    Students who want to study at a university abroad can gain the right to directly transfer to hundreds of prestigious universities around the world with Pathway Preparation Programs, if they do not have the required language level, grade point average or the required score in the proficiency exams for this education program. You can get detailed information from our consultants to take your place in the privileged world of university programs abroad.
+    
+    You can always contact our consultants for information or for your questions.`,
+  },
+  {
+    label: "Postgraduate",
+    code: "postgraduate",
+    type: "Long term",
+    desc: `Master's education abroad has been among the most popular programs in the field of education abroad in our country in recent years. One of the main reasons why postgraduate education abroad is demanded by the increasing number of students in our country is that students see a master's degree abroad as the most important step before their careers in business life.
+
+    America, Canada, England and Germany are among the main countries where one wants to study for a master's degree abroad. The main factors why these countries are preferred for a master's degree abroad are that they offer opportunities such as receiving a world-class master's degree, the opportunity to live and study for a long time in the world's leading cities, and internship and job opportunities.
+    
+    Students who want to study for a master's degree abroad see Canada and Germany as the two prominent countries if they conduct detailed research to receive the highest quality education at the most affordable price. Master's programs in America and England, the two countries that come to mind when it comes to master's degrees abroad, have recently lost their popularity to Canada and Germany due to their costs. Canada and Germany, which are countries that offer postgraduate education abroad in English with their top 500 universities in the world, are in demand by international students thanks to their easy entry conditions and extensive education programs.
+    
+    Entry Conditions
+    Entry requirements to universities for postgraduate study abroad vary from country to country, but generally applied conditions are:
+    
+     Adequate Grade Point Average (University Graduation Average/GPA)
+    
+     Applying to a department in fields parallel to the department studied at the university
+    
+     To show sufficient success in the language exam determined by the school where the master's degree will be received (IELTS / TOEFL-DSH, TestDaf, etc.)
+    
+     Demonstrate the required success in the proficiency exams determined by the school where the master's degree will be received (GRE / GMAT and school-specific exams)
+    
+     Reference Letters (Received from the educational institution you graduated from or your last workplace)
+    
+     Letter of Intent
+    
+     Candidate's CV
+    
+     The candidate's previous achievements, awards and competition results
+    
+    Students who want to study for a Master's degree abroad can gain the right to directly transfer to hundreds of prestigious universities around the world with Pre-Master/Preparatory Programs, if they do not have the required language level, grade point average or the required score in the proficiency exams for this education program. You can get detailed information from our consultants to take your place in the privileged world of master's programs abroad.
+    
+    You can always contact our consultants for information or for your questions.`,
+  },
+  {
+    label: "High Schools",
+    code: "high-schools",
+    type: "Short term & Long term",
+    desc: `HIGH SCHOOL EXCHANGE PROGRAMS
+
+    The international high school exchange program is a program developed and supported in Turkey for years for students who want to develop their vision, have a comprehensive overseas experience, improve themselves by combining domestic and international education experiences, and contribute to their country. With the programs, which can mostly be held for 1 academic year, or sometimes for 1 academic semester (5 months) depending on student demand, students not only improve their academic knowledge through high school education in different parts of the world, but also increase their awareness of responsibility, strengthen their self-confidence and have the chance to expand their worldview.
+    
+    Students who successfully complete their exchange programs have the chance to receive their equivalence by meeting with the Ministry of Education and their schools when they return to Turkey.
+    
+     
+    
+    LONG TERM HIGH SCHOOL PROGRAMS
+    
+    It is a program that offers education opportunities in countries and schools with high academic success for students who want to go to higher education institutions abroad and plan their future abroad. In long-term programs, students often plan to graduate abroad. Although graduation requirements vary from country to country, many countries allow international students the right to graduate. While it is generally necessary to study abroad for a minimum of 2 years to achieve graduation, in some schools it is also possible to graduate in a single year. Turkish students can spend their entire high school life abroad if they wish.
+    
+    Students who receive high school graduation abroad can continue their education abroad by meeting the requirements of the countries and universities they target for their undergraduate education. Students who want to return to Turkey and study at university without taking the exam are required to have completed their entire high school education abroad.
+    
+     
+    
+    General Application Conditions:
+    
+     
+    
+    The student's average in the last 2 years is higher than 70
+    
+    Obtaining a score of 220 or above from the ELTiS English exam – ensuring the required language proficiency according to the preferred country
+    
+    Successful completion of the oral interview
+    
+     
+    
+    ELTiS (English Language Test for International Students) Sınavı:
+    
+     
+    
+    Students who will receive high school education abroad are asked to take a language proficiency test to prove that they can understand English lessons there. ELTiS exam is one of the official exams accepted by these schools for qualification. Within the ELTiS exam, students are subjected to English tests, listening and speaking exams. Speaking exam is carried out by our native English speaking teachers.
+    
+    Our students who will receive high school education in Germany, Italy and France are expected to meet the language proficiency specified by the programs.
+    
+     
+    
+    Program Features:
+    
+     
+    
+    Public and private school options are offered for our students.
+    
+    Students can study in public and private high schools for 1 semester, 1 academic year or more than 1 year.
+    
+    Students fully adapt to the culture by staying with a host family or in school dormitories.
+    
+    Students' vision improves and their personal development is ensured.
+    
+    Students who have high school experience abroad or who are eligible for high school graduation in the countries they visit have an advantageous position in entering universities in these countries.
+    
+     
+    
+    Countries Where Students Can Get High School Education:
+    
+    You have
+    
+    America
+    
+    England
+    
+    Germany
+    
+    Italy
+    
+    France
+    
+    You can always contact our consultants for information or for your questions.`,
+  },
+  {
+    label: "CELTA",
+    code: "celta",
+    type: "Short term",
+    desc: `CELTA teacher training is an English Language Teaching Certificate that is valid and recognized all over the world. Designed by Cambridge's English Language Assesment to help teachers reach their maximum potential. CELTA is considered the proficiency certificate of English teachers in many places. With this certificate, our English teachers are entitled to provide education all over the world. Whether you have just started your career or you are an experienced teacher, this certificate can open the doors of a new world for you.
+
+ 
+
+    More than 10,000 teachers receive the CELTA training certificate every year. The age limit for this training is 20. CELTA is awarded in many countries and cities, the countries we particularly work with are:
+    
+    İngiltere (London, Bournemouth, Oxford, Cambridge..)
+    
+    United States (New York, Los Angeles, San Francisco..)
+    
+    Canada (Toronto, Vancouver..)
+    
+     
+    
+    Training is full-time between 09:00 and 17:30.
+    
+    In addition, for those who work or want to receive training in the evening, there are also part-time trainings between 18:15 and 21:30. 
+    
+     
+    
+    You can always contact our consultants for information or for your questions.`,
+  },
+  {
+    label: "Elderly Programmes",
+    code: "elderly-programmes",
+    type: "Short term",
+    desc: `There is no age limit for learning. Many language schools prepare programs specifically targeting those aged 50 and over. Learning this education by experiencing it together with the culture of the country you visit may be the best thing you can do for your foreign language. 
+
+ 
+
+    These programs consist of classes that appeal to various levels, from those who do not know a foreign language at all to those who know it at a very high level. The training is half-day, and the rest of the day consists of cultural trips in/out of town, sports activities and entertainment activities.
+    
+     
+    
+    Programs for those over the age of 50 are programs of minimum 2 weeks and maximum 1 month.
+    
+    Countries and cities where we have programs,
+    
+    England (London, Oxford, Bournemouth)
+    
+    America (New York)
+    
+    Canada (Toronto)
+    
+     
+    
+    You can always contact our consultants for information or for your questions.`,
+  },
+];
