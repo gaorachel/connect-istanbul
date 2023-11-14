e1fcb1a8c5bea25322473a8a550d6aa2ff24db3a
Rachel <gggggrea@gmail.com>
2023-11-14 00:36:55 +0000

    Add style on ImageSlider & Gallery

--- app/(home)/(routes)/gallery/page.tsx
+++ app/(home)/(routes)/gallery/page.tsx
@@ -1,6 +1,34 @@
+"use client";
+
+import { useMediaQuery } from "@react-hook/media-query";
+
 import { ImageSlider } from "@/components/image-slider";
+import londonSummerSchool202301 from "@/public/gallery/london-summer-school-2023-1.jpg";
+import londonSummerSchool202302 from "@/public/gallery/london-summer-school-2023-2.jpg";
+import londonSummerSchool202303 from "@/public/gallery/london-summer-school-2023-3.jpg";
+import londonSummerSchool202304 from "@/public/gallery/london-summer-school-2023-4.jpg";
+import londonSummerSchool202305 from "@/public/gallery/london-summer-school-2023-5.jpg";
+import londonSummerSchool202306 from "@/public/gallery/london-summer-school-2023-6.jpg";
+import londonSummerSchool202307 from "@/public/gallery/london-summer-school-2023-7.jpg";
+import londonSummerSchool202308 from "@/public/gallery/london-summer-school-2023-8.jpg";
+import londonSummerSchool202309 from "@/public/gallery/london-summer-school-2023-9.jpg";
+import londonSummerSchool202310 from "@/public/gallery/london-summer-school-2023-10.jpg";
+import londonSummerSchool202311 from "@/public/gallery/london-summer-school-2023-11.jpg";
+import torontoSummerSchool202301 from "@/public/gallery/toronto-summer-school-2023-1.jpg";
+import torontoSummerSchool202302 from "@/public/gallery/toronto-summer-school-2023-2.jpg";
+import torontoSummerSchool202303 from "@/public/gallery/toronto-summer-school-2023-3.jpg";
+import torontoSummerSchool202304 from "@/public/gallery/toronto-summer-school-2023-4.jpg";
+import torontoSummerSchool202305 from "@/public/gallery/toronto-summer-school-2023-5.jpg";
+import torontoSummerSchool202306 from "@/public/gallery/toronto-summer-school-2023-6.jpg";
+import torontoSummerSchool202307 from "@/public/gallery/toronto-summer-school-2023-7.jpg";
+import torontoSummerSchool202308 from "@/public/gallery/toronto-summer-school-2023-8.jpg";
+import torontoSummerSchool202309 from "@/public/gallery/toronto-summer-school-2023-9.jpg";
+
+import { Calendar, MapPin } from "lucide-react";
 
 export default function GalleryPage() {
+  const isMediumDevice = useMediaQuery("only screen and (min-width: 768px)");
+
   const trips = [
     {
       name: "London, England - Summer School",
@@ -8,37 +36,37 @@ export default function GalleryPage() {
       date: "June 2023 - July 2023",
       images: [
         {
-          src: "/gallery/london-summer-school-2023-1.jpg",
+          src: londonSummerSchool202301,
         },
         {
-          src: "/gallery/london-summer-school-2023-2.jpg",
+          src: londonSummerSchool202302,
         },
         {
-          src: "/gallery/london-summer-school-2023-3.jpg",
+          src: londonSummerSchool202303,
         },
         {
-          src: "/gallery/london-summer-school-2023-4.jpg",
+          src: londonSummerSchool202304,
         },
         {
-          src: "/gallery/london-summer-school-2023-5.jpg",
+          src: londonSummerSchool202305,
         },
         {
-          src: "/gallery/london-summer-school-2023-6.jpg",
+          src: londonSummerSchool202306,
         },
         {
-          src: "/gallery/london-summer-school-2023-7.jpg",
+          src: londonSummerSchool202307,
         },
         {
-          src: "/gallery/london-summer-school-2023-8.jpg",
+          src: londonSummerSchool202308,
         },
         {
-          src: "/gallery/london-summer-school-2023-9.jpg",
+          src: londonSummerSchool202309,
         },
         {
-          src: "/gallery/london-summer-school-2023-10.jpg",
+          src: londonSummerSchool202310,
         },
         {
-          src: "/gallery/london-summer-school-2023-11.jpg",
+          src: londonSummerSchool202311,
         },
       ],
     },
@@ -48,42 +76,49 @@ export default function GalleryPage() {
       date: "June 2023 - July 2023",
       images: [
         {
-          src: "/gallery/toronto-summer-school-2023-1.jpg",
+          src: torontoSummerSchool202301,
         },
         {
-          src: "/gallery/toronto-summer-school-2023-2.jpg",
+          src: torontoSummerSchool202302,
         },
         {
-          src: "/gallery/toronto-summer-school-2023-3.jpg",
+          src: torontoSummerSchool202303,
         },
         {
-          src: "/gallery/toronto-summer-school-2023-4.jpg",
+          src: torontoSummerSchool202304,
         },
         {
-          src: "/gallery/toronto-summer-school-2023-5.jpg",
+          src: torontoSummerSchool202305,
         },
         {
-          src: "/gallery/toronto-summer-school-2023-6.jpg",
+          src: torontoSummerSchool202306,
         },
         {
-          src: "/gallery/toronto-summer-school-2023-7.jpg",
+          src: torontoSummerSchool202307,
         },
         {
-          src: "/gallery/toronto-summer-school-2023-8.jpg",
+          src: torontoSummerSchool202308,
         },
         {
-          src: "/gallery/toronto-summer-school-2023-9.jpg",
+          src: torontoSummerSchool202309,
         },
       ],
     },
   ];
 
   return (
-    <div>
+    <div className="m-auto w-[500px] md:w-[1000px]">
       {trips.map((trip, index) => (
-        <div key={index} className="p-10">
-          <h1> {trip.name} </h1>
-          <h2> {trip.date} </h2>
+        <div key={index} className="p-5 md:p-10 md:px-20 flex flex-col space-y-1 md:space-y-3 ">
+          <div className="flex flex-row items-baseline gap-2 md:pl-16">
+            <MapPin size={isMediumDevice ? 30 : 20} />
+            <h1 className="text-2xl md:text-3xl font-semibold">{trip.name}</h1>
+          </div>
+          <div className="flex flex-row items-baseline gap-2 md:pl-20">
+            <Calendar size={isMediumDevice ? 20 : 15} color="#71717a" />
+            <h2 className="md:text-lg text-zinc-500"> {trip.date} </h2>
+          </div>
+
           <ImageSlider images={trip.images} name={trip.name} />
         </div>
       ))}
--- components/image-slider.tsx
+++ components/image-slider.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, Circle, Dot } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { useMediaQuery } from "@react-hook/media-query";
 
 interface ImageSliderProps {
-  images: { src: string }[];
+  images: { src: StaticImageData }[];
   name: string;
 }
 
 export const ImageSlider = ({ images, name }: ImageSliderProps) => {
   const [shownImageIndex, setShownImageIndex] = useState(0);
+  const isMediumDevice = useMediaQuery("only screen and (min-width: 768px)");
 
   const handleClickPrevImage = () => {
     const isFirstImage = shownImageIndex === 0;
@@ -33,20 +35,45 @@ export const ImageSlider = ({ images, name }: ImageSliderProps) => {
   };
 
   return (
-    <div className="flex flex-row">
-      <ChevronLeft onClick={handleClickPrevImage} />
-      {images.map((image, index) => (
-        <Image
-          onClick={() => goToImage(index)}
-          src={image.src}
-          alt={`${name} - ${index}`}
-          width={500}
-          height={500}
-          key={index}
-          className={cn(index === shownImageIndex ? "" : "hidden")}
+    <div className="flex flex-col">
+      <div className="flex flex-row m-auto items-center space-y-2">
+        <ChevronLeft
+          onClick={handleClickPrevImage}
+          className="bg-zinc-200 hover:bg-zinc-400/80 transition cursor-pointer rounded-full"
+          size={45}
         />
-      ))}
-      <ChevronRight onClick={handleClickNextImage} />
+        {images.map((image, index) => (
+          <Image
+            onClick={() => goToImage(index)}
+            src={image.src}
+            alt={`${name} - ${index}`}
+            // width={isMediumDevice ? 500 : 300}
+            // height={isMediumDevice ? 500 : 400}
+            key={index}
+            className={cn(
+              index === shownImageIndex
+                ? "m-auto object-contain w-[400px] h-[250px] md:w-[800px] md:h-[500px]"
+                : "hidden"
+            )}
+            priority
+          />
+        ))}
+        <ChevronRight
+          onClick={handleClickNextImage}
+          className="bg-zinc-200 hover:bg-zinc-400/80 transition cursor-pointer rounded-full"
+          size={45}
+        />
+      </div>
+      <div className="flex flex-row m-auto p-2 md:p-4">
+        {images.map((_, index) => (
+          <Dot
+            color={index === shownImageIndex ? "#ef4444" : "#a1a1aa"}
+            size={isMediumDevice ? 40 : 30}
+            strokeWidth={3}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
 };
