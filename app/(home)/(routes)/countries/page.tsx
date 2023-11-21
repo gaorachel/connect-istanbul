6e09be58c4d085fe42ee4db493cf8b4fb407f4a9
Rachel <gggggrea@gmail.com>
2023-11-21 23:01:25 +0000

    Embed world map with dummy data

--- app/(home)/(routes)/countries/page.tsx
+++ app/(home)/(routes)/countries/page.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+"use client";
+
+import { Map } from "./_components/map";
+
+import { data } from "./_data/mapGenerationData";
+import { connectionData } from "./_data/connectionData";
 
 export default function CountriesPage() {
-  return <div>CountriesPage</div>;
+  return (
+    <div className="w-[900px]">
+      You can study in many countries!
+      <div className="m-auto h-full"></div>
+      <Map width={1000} height={800} data={data} connectionData={connectionData} />
+    </div>
+  );
 }
